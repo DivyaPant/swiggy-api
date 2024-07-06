07b7cef70786e146c87dbbba122227ce3b9bf151
Divya <divyapant369@gmail.com>
2024-07-06 11:38:00 +0530

    add to remote

--- App.js
+++ App.js
@@ -15,25 +15,73 @@ import ReactDOM from "react-dom/client";
 
 // console.log(parent); // object
 
-const jsxHeading = <h1>Heading from jsx</h1> // React element
-const Title = ()=> {
-    return <div>Title</div>
+// const jsxHeading = <h1>Heading from jsx</h1> // React element
+// const Title = ()=> {
+//     return <div>Title</div>
+// }
+
+// const HeadingComponent = ()=>{
+//     return (
+//         <>
+//         <Title/>
+//         <Title></Title>
+//     {jsxHeading}   {/* write js in jsx using {} */}
+//     {Title()} {/* Fun components are also functions after all!! */}
+//     {console.log('anything')}
+//     <h1>Heading from Functional Component</h1>
+//     </>
+//     );
+// }
+
+const Header = ()=>{
+    return (
+    <div className="header">
+        <div className="logo-container">
+            <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png' alt="Logo"/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Name</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+)}
+
+const RestaurantCard = ()=>{
+    return (
+        <div className="res-card">
+             <img className='card-img'
+             src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/%22Hyderabadi_Dum_Biryani%22.jpg/800px-%22Hyderabadi_Dum_Biryani%22.jpg' alt='res-img'/>
+            <h3>Meghana Foods</h3>
+           <h4>Biryani</h4>
+           <h4>4.4 stars</h4>
+           <h4>38 mins</h4>
+        </div>
+    );
 }
 
-const HeadingComponent = ()=>{
+const Body = ()=> {
     return (
-        <>
-        <Title/>
-        <Title></Title>
-    {jsxHeading}   {/* write js in jsx using {} */}
-    {Title()} {/* Fun components are also functions after all!! */}
-    {console.log('anything')}
-    <h1>Heading from Functional Component</h1>
-    </>
-    );
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = ()=> {
+    return <div className="app">
+<Header/>
+<Body />
+    </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(jsxHeading);
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
---
+++ Notes.js
@@ -0,0 +1,18 @@
+/* 
+Food Ordering App Design/Layout
+Header 
+- Logo
+- Nav Items
+Body
+- Search 
+- Card Container
+  - Card 
+    - Image
+    - Name
+    - Star Rating , Cuisine, delivery time, etc
+Footer
+- Copyyright
+- Links
+- Address
+- Contact
+*/
